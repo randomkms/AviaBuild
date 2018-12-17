f42e92efb523215a25116ccf41b89b2ebaa15675
Maxim Kiyan <sp115-18@stu.zsea.edu.ua>
2018-12-17 17:11:16 +0200

    init commit

---
+++ AviaBuild/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace AviaBuild
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var vCulture = CultureInfo.InvariantCulture;
+
+            Thread.CurrentThread.CurrentCulture = vCulture;
+            Thread.CurrentThread.CurrentUICulture = vCulture;
+            CultureInfo.DefaultThreadCurrentCulture = vCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = vCulture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            base.OnStartup(e);
+        }
+    }
+}
